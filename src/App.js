deddeab6f4f20787305d86db798c2f927e906f02
KaviArasuNS <KaviArasu_NS@icloud.com>
2022-07-15 20:12:11 +0530

    react mobiles display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+      const API = "http://localhost:4000";
+
+//       const mobiles = [
+//   {
+//     model: "OnePlus 9 5G",
+//     img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+//     company: "Oneplus"
+//   },
+//   {
+//     model: "Iphone 13 mini",
+//     img:
+//       "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+//     company: "Apple"
+//   },
+//   {
+//     model: "Samsung s21 ultra",
+//     img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+//     company: "Samsung"
+//   },
+//   {
+//     model: "Xiomi mi 11",
+//     img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+//     company: "Xiomi"
+//   }
+// ];
+    const [mobiles, setMobiles] = useState([]);
+
+  const getMobiles = () => {
+      fetch(`${API}/mobiles`).then(data => data.json()).then(mobiles => setMobiles(mobiles));
+  }
+
+   useEffect(()=> getMobiles,[])
+
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="phone-list-container">
+    {mobiles.map((mobile) => <Phone key={mobile._id} mobile={mobile}/>)}
+    </div> 
   );
 }
 
+
 export default App;
+
+  function Phone({mobile}){
+  return(
+      <div className="phone-container">
+      <img className='phone-picture' src={mobile.img} alt={mobile.model}/>
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
+    </div>
+    )
+    }
