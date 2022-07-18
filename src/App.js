a416a1caab20376148d42bf7c7803d2afab790fe
KaviArasuNS <KaviArasu_NS@icloud.com>
2022-07-18 16:13:45 +0530

    update crud operation

--- src/App.js
+++ src/App.js
@@ -2,45 +2,46 @@ import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
-    const mobile = [
-  {
-    model: "OnePlus 9 5G",
-    img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    company: "Oneplus"
-  },
-  {
-    model: "Iphone 13 mini",
-    img:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-    company: "Apple"
-  },
-  {
-    model: "Samsung s21 ultra",
-    img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-    company: "Samsung"
-  },
-  {
-    model: "Xiomi mi 11",
-    img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-    company: "Xiomi"
+//     const mobile = [
+//   {
+//     model: "OnePlus 9 5G",
+//     img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+//     company: "Oneplus"
+//   },
+//   {
+//     model: "Iphone 13 mini",
+//     img:
+//       "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+//     company: "Apple"
+//   },
+//   {
+//     model: "Samsung s21 ultra",
+//     img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+//     company: "Samsung"
+//   },
+//   {
+//     model: "Xiomi mi 11",
+//     img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+//     company: "Xiomi"
+//   }
+// ];
+
+
+      const API = "https://kavi-node-app.herokuapp.com";
+
+    const [mobiles, setMobiles] = useState([]);
+
+  const getMobiles = () => {
+      fetch(`${API}/mobiles`,{method: "GET"}
+      ).then(data => data.json()).then(mobiles => setMobiles(mobiles));
   }
-];
 
-
-  //     const API = "https://kavi-node-app.herokuapp.com";
-
-  //   const [mobiles, setMobiles] = useState([]);
-
-  // const getMobiles = () => {
-  //     fetch(`${API}/mobiles`).then(data => data.json()).then(mobiles => setMobiles(mobiles));
-  // }
-
-  //  useEffect(()=> getMobiles,[])
+   useEffect(()=> getMobiles,[])
 
       
   return (
     <div className="phone-list-container">
-    {mobile.map((mobile) => <Phone key={mobile._id} mobile={mobile}/>)}
+    {mobiles.map((mobile) => <Phone key={mobile._id} mobile={mobile}/>)}
     </div> 
   );
 }
